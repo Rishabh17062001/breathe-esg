00aefa656d41349d8c0d10f186c318fdcfe586dd
Rishabh17062001 <rishabh17062001@gmail.com>
2026-05-26 00:50:53 +0530

    Fix CSRF error for REST API actions

--- backend/breatheesg/settings.py
+++ backend/breatheesg/settings.py
@@ -98,6 +98,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser',
         'rest_framework.parsers.FormParser',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
 }
